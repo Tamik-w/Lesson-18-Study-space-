01bf04a5c2395aa36efc5d72ef87b6612277da5c
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-02-12 05:03:27 +0300

    final result

---
+++ Task 1-2/js/app.js
@@ -0,0 +1,11 @@
+let firstSalary = prompt('Enter first month salary: ');
+
+let secondSalary = prompt('Enter second month salary: ');
+
+let salary = +firstSalary + +secondSalary;
+
+alert(`Your salary for 2 months is: ${salary}$`);
+
+let salaryBonus = ++salary;
+
+alert(`You have extra bonus! Your total salary was: ${salary}$ it's increase for 1$ and now it is: ${salaryBonus}$`);
---
+++ Task 4/js/app.js
@@ -0,0 +1,18 @@
+let firstSalary = prompt('Enter first month salary: ');
+
+let secondSalary = prompt('Enter second month salary: ');
+
+let salary = +firstSalary + +secondSalary;
+
+alert(`Your salary for 2 months is: ${salary}$`);
+
+let salaryBonus = ++salary;
+
+alert(`You have extra bonus! Your total salary was: ${salary}$ it's increase for 1$ and now it is: ${salaryBonus}$`);
+
+if(salaryBonus >= 2000){
+    alert(`I'm ready to work!`)
+}
+else{
+    alert(`I'm out!`)
+}
